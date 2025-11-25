4f1530ce0611ed386b617f37fcf8b8b26b2ed9b5
LouisChristopher-001 <louischristopher784@gmail.com>
2025-11-25 16:47:51 +0530

    cors

--- sales-target-frontend/src/api.js
+++ sales-target-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://target-qsx5.onrender.com" // your backend URL
+  baseURL: "https://https://target-5k2w.onrender.com" // your backend URL
 });
 
 export default api;
