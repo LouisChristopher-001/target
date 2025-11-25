75e60882addfd5d924dafbf68137ebe5d47db202
LouisChristopher-001 <louischristopher784@gmail.com>
2025-11-25 16:23:58 +0530

    deploy

--- sales-target-frontend/src/api.js
+++ sales-target-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000" // your backend URL
+  baseURL: "https://target-qsx5.onrender.com" // your backend URL
 });
 
 export default api;
